204870ebadb62f76bba870cc81d12818865b10f0
Anna Kight <anna.kight@gmail.com>
2023-12-05 21:52:29 -0700

    Day 4 - Part 2

--- day_4_Scratchcards/day_4.go
+++ day_4_Scratchcards/day_4.go
@@ -5,20 +5,48 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
+
+type card struct {
+	number     int
+	count      int
+	points     int
+	matchCount int
+}
 
 func main() {
 	file, fileScanner := getFileScanner("input.txt")
 
 	totalPoints := 0
+	var cards []card
 	for fileScanner.Scan() {
-		card := fileScanner.Text()
-		points := calculatePoints(card)
+		cardData := fileScanner.Text()
+		number, points, matches := calculatePoints(cardData)
+		newCard := card{
+			number:     number,
+			count:      1,
+			points:     points,
+			matchCount: matches,
+		}
+		cards = append(cards, newCard)
 
 		totalPoints += points
 	}
 	fmt.Println("The point total for all the cards is: ", fmt.Sprint(totalPoints))
+
+	totalCards := 0
+	for i, current := range cards {
+		for n := 1; n <= current.matchCount; n++ {
+			cards[i+n].count += current.count
+		}
+	}
+	for _, card := range cards {
+		totalCards += card.count
+	}
+
+	fmt.Println("The total number of scratchcards is: ", fmt.Sprint(totalCards))
 
 	file.Close()
 }
@@ -35,15 +63,18 @@ func getFileScanner(fileName string) (*os.File, *bufio.Scanner) {
 	return file, fileScanner
 }
 
-func calculatePoints(card string) (points int) {
+func calculatePoints(card string) (cNumber int, points int, matches int) {
 	points = 0
+	matches = 0
 	digits := regexp.MustCompile("[0-9]+")
 	splitCardNum := strings.Split(card, ":")
+	cNumber, _ = strconv.Atoi(strings.Trim(splitCardNum[0], "Card "))
 	numbers := strings.Split(splitCardNum[1], "|")
 	winningNumbers := digits.FindAllString(numbers[0], -1)
 	numbersElfHas := digits.FindAllString(numbers[1], -1)
 	for _, elfNum := range numbersElfHas {
 		if isWinner(winningNumbers, elfNum) {
+			matches++
 			if points == 0 {
 				points = 1
 			} else {
@@ -51,7 +82,7 @@ func calculatePoints(card string) (points int) {
 			}
 		}
 	}
-	return points
+	return cNumber, points, matches
 }
 
 func isWinner(winNumbers []string, have string) bool {
