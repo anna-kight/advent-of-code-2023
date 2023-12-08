c80c591f8ab08f7026afaae9f41d062ebfb243eb
Anna Kight <anna.kight@gmail.com>
2023-12-07 23:09:55 -0700

    Day 5 - Part 2

--- day_5_If_You_Give_A_Seed_A_Fertilizer/day_5.go
+++ day_5_If_You_Give_A_Seed_A_Fertilizer/day_5.go
@@ -15,17 +15,19 @@ type almanacMap struct {
 	conversions    [][3]int
 }
 
+//69841804 too high
+
 func main() {
 
 	var almanac []almanacMap
 	var currentMap almanacMap
-	var seeds []int
+	var seedRanges []int
 	digits := regexp.MustCompile("[0-9]+")
-	file, fileScanner := getFileScanner("example.txt")
+	file, fileScanner := getFileScanner("input.txt")
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
 		if strings.Index(line, "seeds:") == 0 {
-			seeds = getSeeds(line)
+			seedRanges = getSeedRanges(line)
 		} else if strings.Contains(line, ":") {
 			if currentMap.sourceCat != "" {
 				almanac = append(almanac, currentMap)
@@ -38,13 +40,18 @@ func main() {
 			currentMap.conversions = append(currentMap.conversions, newConversion)
 		}
 	}
+	almanac = append(almanac, currentMap)
 	file.Close()
-	var lowestLocation int
+	lowestLocation := -2
 
-	for _, seed := range seeds {
-		location := getLocation(almanac, seed)
-		if location < lowestLocation {
-			lowestLocation = location
+	for i := 0; i < len(seedRanges)-1; i = i + 2 {
+		seeds := getSeeds(seedRanges[i : i+2])
+
+		for _, seed := range seeds {
+			location := getLocation(almanac, seed)
+			if location < lowestLocation || lowestLocation == -2 {
+				lowestLocation = location
+			}
 		}
 	}
 
@@ -63,13 +70,31 @@ func getFileScanner(fileName string) (*os.File, *bufio.Scanner) {
 	return file, fileScanner
 }
 
-func getSeeds(line string) (nums []int) {
+func getSeeds(seedRange []int) (nums []int) {
+	// seeds := strings.Split(line, " ")
+	// seeds = seeds[1:]
+	// var seedRange []int
+	// for _, seed := range seeds {
+	// 	temp, _ := strconv.Atoi(seed)
+	// 	seedRange = append(seedRange, temp)
+	// }
+	for i := 0; i < len(seedRange)-1; i = i + 2 {
+		for j := 0; j < seedRange[i+1]; j++ {
+			nums = append(nums, seedRange[i]+j)
+		}
+	}
+
+	return nums
+}
+
+func getSeedRanges(line string) (seedRange []int) {
 	seeds := strings.Split(line, " ")
 	seeds = seeds[1:]
-	for index, seed := range seeds {
-		nums[index], _ = strconv.Atoi(seed)
+	for _, seed := range seeds {
+		temp, _ := strconv.Atoi(seed)
+		seedRange = append(seedRange, temp)
 	}
-	return nums
+	return seedRange
 }
 
 func lineToMap(line string) (rangeMap [3]int) {
@@ -81,5 +106,37 @@ func lineToMap(line string) (rangeMap [3]int) {
 }
 
 func getLocation(almanac []almanacMap, seed int) (location int) {
+	cat := "seed"
+	num := seed
+	for i := 0; i < 7; i++ {
+		amap := findNextMap(almanac, cat)
+		num = sourceToDest(num, amap)
+		cat = amap.destinationCat
+	}
+	if cat == "location" {
+		location = num
+	} else {
+		location = -1
+	}
+
 	return location
+}
+
+func findNextMap(almanac []almanacMap, cat string) almanacMap {
+	for _, amap := range almanac {
+		if cat == amap.sourceCat {
+			return amap
+		}
+	}
+	return almanacMap{}
+}
+
+func sourceToDest(num int, amap almanacMap) int {
+	for _, con := range amap.conversions {
+		if con[1] <= num && num <= con[1]+con[2] {
+			offset := num - con[1]
+			return con[0] + offset
+		}
+	}
+	return num
 }
