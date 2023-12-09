4fb42273b30fb1b7273587ca046eccef242a8fd4
Anna Kight <anna.kight@gmail.com>
2023-12-09 13:57:25 -0700

    Day 8 - Part 2 - attempt

--- day_8_Haunted_Wasteland/day_8.go
+++ day_8_Haunted_Wasteland/day_8.go
@@ -18,9 +18,9 @@ func main() {
 	var instructions string
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		if !strings.Contains(line, "=") {
+		if !strings.Contains(line, "=") && strings.Contains(line, "L") {
 			instructions = line
-		} else {
+		} else if strings.Contains(line, "=") {
 			split := strings.Split(line, " = ")
 			key := split[0]
 			next := strings.Trim(split[1], "()")
@@ -28,21 +28,16 @@ func main() {
 			val := nextNode{L: nextSplit[0], R: nextSplit[1]}
 			nodes[key] = val
 		}
-
 	}
 	file.Close()
 	steps := 0
-	currentNode := "AAA"
+	currentNodes := getStartingNodes(nodes)
 	i := 0
-	for currentNode != "ZZZ" {
+	for !allNodesEndInZ(currentNodes) {
 		if i >= len(instructions) {
 			i = 0
 		}
-		if instructions[i] == 'L' {
-			currentNode = nodes[currentNode].L
-		} else {
-			currentNode = nodes[currentNode].R
-		}
+		currentNodes = getNextNodes(currentNodes, nodes, instructions[i])
 		steps++
 		i++
 	}
@@ -60,4 +55,34 @@ func getFileScanner(fileName string) (*os.File, *bufio.Scanner) {
 
 	fileScanner.Split(bufio.ScanLines)
 	return file, fileScanner
+}
+
+func getStartingNodes(nodeMap map[string]nextNode) (sNodes []string) {
+	for name, _ := range nodeMap {
+		if name[len(name)-1:] == "A" {
+			sNodes = append(sNodes, name)
+		}
+	}
+	return sNodes
+}
+
+func allNodesEndInZ(nodes []string) bool {
+	for _, n := range nodes {
+		if n[len(n)-1:] != "Z" {
+			return false
+		}
+	}
+	return true
+}
+
+func getNextNodes(nodes []string, nodeMap map[string]nextNode, direction byte) (nNodes []string) {
+	nNodes = nodes
+	for i, n := range nodes {
+		if direction == 'L' {
+			nNodes[i] = nodeMap[n].L
+		} else {
+			nNodes[i] = nodeMap[n].R
+		}
+	}
+	return nNodes
 }
